88615048e2293e0961f71ce104fb386e9035a851
a-mhamdi <a_mhamdi@outlook.com>
2025-11-05 21:19:47 +0100

    new updates

--- ros2_ws/src/listen_talk_pkg/listen_talk_pkg/listen.py
+++ ros2_ws/src/listen_talk_pkg/listen_talk_pkg/listen.py
@@ -1,25 +1,29 @@
 import rclpy
 from std_msgs.msg import String
 
+
 def callback(msg):
+    """Callback function for the subscriber"""
     print('[LISTENER] "%s"' % msg.data)
 
-def main(args=None):
-    rclpy.init(args=args)
 
-    node = rclpy.create_node('my_subscriber')
+def main(args=None):
+    rclpy.init(args=args)  # Initialize ROS2
+
+    node = rclpy.create_node("my_subscriber")  # Create a node
+
+    # Create a subscription that listens to messages of type String on the 'listen_talk' topic with a queue size of 10
     subscriber = node.create_subscription(
         String,
-        'listen_talk',
+        "listen_talk",
         callback,
-        10
+        10,  # QoS profile
     )
+    rclpy.spin(node)  # Spin the node
 
-    while rclpy.ok():
-        rclpy.spin_once(node)
+    node.destroy_node()  # Destroy the node
+    rclpy.shutdown()  # Shutdown ROS2
 
-    node.destroy_node()
-    rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- ros2_ws/src/listen_talk_pkg/listen_talk_pkg/talk.py
+++ ros2_ws/src/listen_talk_pkg/listen_talk_pkg/talk.py
@@ -1,22 +1,31 @@
 import rclpy
 from std_msgs.msg import String
+from time import sleep
+
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init(args=args)  # Initialize ROS2
 
-    node = rclpy.create_node('my_publisher')
-    publisher = node.create_publisher(String, 'listen_talk', 10)
+    node = rclpy.create_node("my_publisher")  # Create a node
+
+    # Create a publisher that will publish messages of type String on the 'listen_talk' topic with a queue size of 10
+    publisher = node.create_publisher(String, "listen_talk", 10)
 
     msg = String()
-    msg.data = "Hello, ROS 2!"
 
+    i = 0
     while rclpy.ok():
-        publisher.publish(msg)
+        i += 1
+        msg.data = f"Hello, ROS 2! (#{str(i)})"
+        publisher.publish(msg)  # Publish the message
         node.get_logger().info('[TALKER] "%s"' % msg.data)
-        rclpy.spin_once(node)
+        sleep(1)
 
-    node.destroy_node()
-    rclpy.shutdown()
+    rclpy.spin(node)  # Spin the node
 
-if __name__ == '__main__':
+    node.destroy_node()  # Destroy the node
+    rclpy.shutdown()  # Shutdown ROS2
+
+
+if __name__ == "__main__":
     main()
